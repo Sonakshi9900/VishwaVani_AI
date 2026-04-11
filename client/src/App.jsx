805e82f3e604c53346227bd3195bb492ebfc2adb
Sonakshi Gupta <sonakshikumari9900@gmail.com>
2026-04-12 00:00:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
-import Dashboard from './Dashboard';
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
